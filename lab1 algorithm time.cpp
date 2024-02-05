ed1cebfc5975f1f154da5a402a7ae6f36e66c697
ShakW101 <157775686+ShakW101@users.noreply.github.com>
2024-02-05 18:37:00 +0530

    Create lab1 algorithm time.cpp

---
+++ lab1 algorithm time.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <random>
+
+using namespace std;
+using namespace chrono;
+
+vector<int> generateRandomVector(int length, int minVal, int maxVal) {
+    // Seed the random number generator
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> distribution(minVal, maxVal);
+
+    // Generate the random vector
+    vector<int> randomVector(length);
+    for (int i = 0; i < length; ++i) {
+        randomVector[i] = distribution(gen);
+    }
+
+    return randomVector;
+}
+
+void outputArray(vector<int> data){
+    int sizeL = data.size();
+    for (int i = 0; i < sizeL; ++i) {
+        cout << data[i];
+        if (i != sizeL - 1) {
+            cout << " ";
+        }
+    }
+    cout << endl;
+}
+
+void bubbleSort(vector<int> inputList){
+    //Your bubble sort algorithm here
+    int sizeL = inputList.size();
+    for(int i = 0; i < sizeL - 1; i++){
+        for(int j = 0; j < sizeL - 1 - i; j++){
+            if(inputList[j] > inputList[j+1]){
+                int temp = inputList[j];
+                inputList[j] = inputList[j+1];
+                inputList[j+1] = temp;
+            }
+        }
+    }
+}
+
+void optimizedBubbleSort(vector<int> inputList){
+    int sizeL = inputList.size();
+
+    for(int i = 0; i < sizeL - 1; i++){
+        //this flag will check if any swaps have taken place
+        bool flag = false;
+        for(int j = 0; j < sizeL - 1 - i; j++){
+            if(inputList[j] > inputList[j+1]){
+                int temp = inputList[j];
+                inputList[j] = inputList[j+1];
+                inputList[j+1] = temp;
+                flag = true;
+            }
+        }
+        //if no swaps occured, simply break the loop
+        if(flag == false){
+            break;
+        }
+    }
+
+}
+
+void insertionSort(vector<int> inputList){
+    int n = inputList.size();
+    for (int i = 1; i < n; ++i) {
+        int key = inputList[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && inputList[j] > key) {
+            inputList[j + 1] = inputList[j];
+            j = j - 1;
+        }
+        inputList[j + 1] = key;
+
+    }
+}
+
+void selectionSort(vector<int> inputList){
+    int sizeL = inputList.size();
+
+    for (int i = 0; i < sizeL - 1; ++i) {
+        // find the minimum element in unsorted array
+        int minIndex = i;
+        for (int j = i + 1; j < sizeL; ++j) {
+            if (inputList[j] < inputList[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // swap the found minimum element with the first element
+        int temp = inputList[i];
+        inputList[i] = inputList[minIndex];
+        inputList[minIndex] = temp;
+    }
+}
+
+
+int main() {
+    int upperLimit = 50;
+    int lowerLimit = 3;
+    int skips = 2;
+    int listLow = 0;
+    int listHigh = 500;
+
+    vector<int> testList;
+
+    void (*algorithms[])(vector<int>) = {bubbleSort, optimizedBubbleSort, insertionSort, selectionSort};
+    int funcsLen = 4;
+    string names[] = {"Bubble Sort", "Optimized Bubble Sort", "Insertion Sort", "Selection Sort"};
+
+    for(int length = lowerLimit; length <= upperLimit; length = length + skips){
+        testList = generateRandomVector(length, listLow, listHigh);
+        cout << "For a list of " << length << " elements, " <<endl;
+        for(int i = 0; i < funcsLen; i++) {
+            duration<double> duration[3];
+            for (int x = 0; x < 3; x++) {
+                auto start = high_resolution_clock::now();
+                algorithms[i](testList);
+                auto end = high_resolution_clock::now();
+                duration[x] = end - start;
+            }
+            ::duration<double> finalDur = (duration[0] + duration[1] + duration[2])/3;
+            cout  << finalDur.count()*1000   << endl;
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
