e7daa878816bf90b96e6ae87507d7e7a2adeea2a
ShakW101 <157775686+ShakW101@users.noreply.github.com>
2024-02-05 18:09:32 +0530

    Create lab1.2.2.cpp

---
+++ lab1.2.2.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int hourglassSum(vector<vector<int>> arr) {
+    // keep track of maximum
+    int maximum;
+    //this will track if the iteration is the first one
+    bool first = true;
+    //iterate through the array as asked
+    for(int x = 0; x <= 3; x++){
+        for(int i = 0; i <= 3; i++){
+            //this variable will keep track of a temporary total
+            int total = 0;
+            for(int y = i; y <= i + 2; y++){
+                total += arr[x][y];
+            }
+            total += arr[x+1][i+1];
+            for(int y = i; y <= i + 2; y++){
+                total += arr[x+2][y];
+            }
+            //this is done to set the maxmimum to any number through the first iteration, even negative ones
+            if(first){
+                maximum = total;
+                first = false;
+            }else{
+                if(total > maximum){
+                    maximum = total;
+                }
+            }
+        }
+            
+        }
+        
+       
+    return maximum;
+    }
+    
+
+
+
+int main()
+{
+    vector<vector<int>> arr(6);
+
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < 6; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+
+    int result = hourglassSum(arr);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
